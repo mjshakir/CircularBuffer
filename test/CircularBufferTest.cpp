0c278e99ee0627af175e4e99b2c72b7f7f954659
mjshakir <maslove23@gmail.com>
2024-05-23 23:37:20 -0700

    Add: move and copy constructor and operator test

--- test/CircularBufferTest.cpp
+++ test/CircularBufferTest.cpp
@@ -3,8 +3,8 @@
 
 #include "CircularBuffer.hpp"
 
-constexpr size_t BUFFER_SIZE = 10;   
-TEST(CircularBufferTypeCheck, ValidateTypes) {
+constexpr size_t BUFFER_SIZE = 10UL;   
+TEST(CircularBufferTast, ValidateTypes) {
     // These checks are compile-time, this test will only run if they pass
     EXPECT_FALSE((std::is_same<CircularBuffer::CircularBuffer<int, BUFFER_SIZE>, CircularBuffer::CircularBufferFixed<int, BUFFER_SIZE>>::value));
     EXPECT_FALSE((std::is_same<CircularBuffer::CircularBuffer<int>, CircularBuffer::CircularBufferDynamic<int>>::value));
@@ -20,4 +20,122 @@ TEST(CircularBufferTypeCheck, ValidateTypes) {
     
     EXPECT_NE(nullptr, (dynamic_cast<CircularBuffer::CircularBufferFixed<int, BUFFER_SIZE>*>(&fixedBuffer)));
     EXPECT_NE(nullptr, (dynamic_cast<CircularBuffer::CircularBufferDynamic<int>*>(&dynamicBuffer)));
+}
+
+
+TEST(CircularBufferTast, CopyConstructorFixed) {
+    CircularBuffer::CircularBuffer<int, 3UL> buffer;
+    buffer.push(1);
+    buffer.push(2);
+    buffer.push(3);
+
+    auto copyBuffer(buffer);
+    EXPECT_EQ(copyBuffer.size(), 3);
+    EXPECT_EQ(copyBuffer.top_pop().value(), 1);
+    EXPECT_EQ(copyBuffer.top_pop().value(), 2);
+    EXPECT_EQ(copyBuffer.top_pop().value(), 3);
+}
+
+TEST(CircularBufferTast, CopyAssignmentOperatorFixed) {
+    CircularBuffer::CircularBuffer<int, 5UL> buffer1;
+    buffer1.push(1);
+    buffer1.push(2);
+    buffer1.push(3);
+
+    // CircularBuffer::CircularBuffer<int, 5> buffer2;
+    auto buffer2 = buffer1;
+    EXPECT_EQ(buffer2.size(), 3);
+    EXPECT_EQ(buffer2.top_pop().value(), 1);
+    EXPECT_EQ(buffer2.top_pop().value(), 2);
+    EXPECT_EQ(buffer2.top_pop().value(), 3);
+}
+
+TEST(CircularBufferTast, MoveConstructorFixed) {
+    CircularBuffer::CircularBuffer<int, 5UL> buffer;
+    buffer.push(1);
+    buffer.push(2);
+    buffer.push(3);
+
+    auto movedBuffer(std::move(buffer));
+    EXPECT_EQ(movedBuffer.size(), 3);
+    EXPECT_EQ(movedBuffer.top_pop().value(), 1);
+    EXPECT_EQ(movedBuffer.top_pop().value(), 2);
+    EXPECT_EQ(movedBuffer.top_pop().value(), 3);
+
+    // buffer should be in a valid state after the move, but its content might be unspecified
+    EXPECT_TRUE(buffer.empty() or !buffer.empty());
+}
+
+TEST(CircularBufferTast, MoveAssignmentOperatorFixed) {
+    CircularBuffer::CircularBuffer<int, 5UL> buffer1;
+    buffer1.push(1);
+    buffer1.push(2);
+    buffer1.push(3);
+
+    auto buffer2 = std::move(buffer1);
+    EXPECT_EQ(buffer2.size(), 3);
+    EXPECT_EQ(buffer2.top_pop().value(), 1);
+    EXPECT_EQ(buffer2.top_pop().value(), 2);
+    EXPECT_EQ(buffer2.top_pop().value(), 3);
+
+    // buffer1 should be in a valid state after the move, but its content might be unspecified
+    EXPECT_TRUE(buffer1.empty() or !buffer1.empty());
+}
+
+TEST(CircularBufferTast, CopyConstructorDynamic) {
+    CircularBuffer::CircularBuffer<int> buffer(3UL);
+    buffer.push(1);
+    buffer.push(2);
+    buffer.push(3);
+
+    auto copyBuffer(buffer);
+    EXPECT_EQ(copyBuffer.size(), 3);
+    EXPECT_EQ(copyBuffer.top_pop().value(), 1);
+    EXPECT_EQ(copyBuffer.top_pop().value(), 2);
+    EXPECT_EQ(copyBuffer.top_pop().value(), 3);
+}
+
+TEST(CircularBufferTast, CopyAssignmentOperatorDynamic) {
+    CircularBuffer::CircularBuffer<int> buffer1(5UL);
+    buffer1.push(1);
+    buffer1.push(2);
+    buffer1.push(3);
+
+    auto buffer2 = buffer1;
+    EXPECT_EQ(buffer2.size(), 3);
+    EXPECT_EQ(buffer2.top_pop().value(), 1);
+    EXPECT_EQ(buffer2.top_pop().value(), 2);
+    EXPECT_EQ(buffer2.top_pop().value(), 3);
+}
+
+TEST(CircularBufferTast, MoveConstructorDynamic) {
+    CircularBuffer::CircularBuffer<int> buffer(5UL);
+    buffer.push(1);
+    buffer.push(2);
+    buffer.push(3);
+
+    auto movedBuffer(std::move(buffer));
+    EXPECT_EQ(movedBuffer.size(), 3);
+    EXPECT_EQ(movedBuffer.top_pop().value(), 1);
+    EXPECT_EQ(movedBuffer.top_pop().value(), 2);
+    EXPECT_EQ(movedBuffer.top_pop().value(), 3);
+
+    // buffer should be in a valid state after the move, but its content might be unspecified
+    EXPECT_TRUE(buffer.empty() or !buffer.empty());
+}
+
+TEST(CircularBufferTast, MoveAssignmentOperatorDynamic) {
+    CircularBuffer::CircularBuffer<int> buffer1(5UL);
+    buffer1.push(1);
+    buffer1.push(2);
+    buffer1.push(3);
+
+    auto buffer2 = std::move(buffer1);
+    EXPECT_EQ(buffer2.size(), 3);
+    EXPECT_EQ(buffer2.top_pop().value(), 1);
+    EXPECT_EQ(buffer2.top_pop().value(), 2);
+    EXPECT_EQ(buffer2.top_pop().value(), 3);
+
+    // buffer1 should be in a valid state after the move, but its content might be unspecified
+    EXPECT_TRUE(buffer1.empty() or !buffer1.empty());
 }
